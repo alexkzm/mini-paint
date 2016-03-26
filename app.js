98aaa1f7cf10f0c9d173556d4ee6fd89b80e80e7
Alexander Kouzmine <alexkzm@gmail.com>
2016-03-26 12:24:45 -0600

    jQuery short project

---
+++ app.js
@@ -0,0 +1,64 @@
+//Assigns colors to boxes
+$(document).ready(function() {
+
+	var box =$('.box');
+
+	$('.box').on('click', function() {
+    $(this).addClass('white');
+  })
+
+  	$('.box').on('dblclick', function() {
+    $(this).removeClass('white');
+  })
+
+    $('#reset').on('click', function() {
+    $('.box').removeClass('white green');
+  })
+//Assigns actions to buttons
+var color = 'white';
+
+$('#red').on('click', function() {
+    color = 'red';
+  })
+
+  $('#blue').on('click', function() {
+    color = 'blue';
+  })
+
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+
+  $('#white').on('click', function() {
+    color = 'white';
+  })
+
+   $('.box').on('click', function() {
+    $(this).addClass(color);
+  })
+
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(color);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(color)
+  })
+})
+
+//Changes color back to black on double click
+ $('.box').removeClass('red blue green yellow white');
+
+ var colors = 'white green red blue yellow';
+
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(colors);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(colors)
+  })
